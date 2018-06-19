f7e02fa6cc068dd3d696d1edbb3f9219c02a1a4f
Sota Hatakeyama <sota.hatakeyama@kabuku.co.jp>
2018-06-19 16:30:06 +0900

    feat: capture image tensor by webcamera

--- web/index.ts
+++ web/index.ts
@@ -1,5 +1,6 @@
+import * as tf from '@tensorflow/tfjs';
 import { fromEvent, interval, asyncScheduler, merge } from 'rxjs';
-import { switchMap, takeUntil, map, mapTo } from 'rxjs/operators';
+import { switchMap, takeUntil, mapTo } from 'rxjs/operators';
 import { Robot, RobotController, Wheel } from './robot';
 
 const velocityInput = document.getElementById('velocityInput') as HTMLInputElement;
@@ -47,26 +48,50 @@ const createPressStream = (el: Element) => fromEvent(el, 'mousedown')
   );
 
 enum Command {
-  Neutral = 'neutral',
-  Forward = 'forword',
-  Backward = 'backword'
+  Neutral = 0,
+  Forward = 1,
+  Backward = 2
 }
 
+/**
+ * Resize the given image tensor to a squred one.
+ */
+const cropImage = (image: tf.Tensor): tf.Tensor => {
+  const [height, width] = image.shape;
+  const size = Math.min(width, height);
+  const start = [(height - size) / 2, (width - size) / 2, 0];
+  const end = [size, size, 3];
+  return image.slice(start, end);
+};
+
+const capture = (webcam: HTMLVideoElement): tf.Tensor => tf.tidy(() => {
+  const webcamImage = tf.fromPixels(webcam);
+  const cropped = cropImage(webcamImage);
+  const expanded = cropped.expandDims();
+  return expanded.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+});
+
+let webcamera: HTMLVideoElement;
+
 const setupUI = () => {
-  const backwardButton = document.querySelector('.backward');
+  webcamera = document.querySelector('#webcam') as HTMLVideoElement;
+  setupWebcamera(webcamera);
+
   const neutralButton = document.querySelector('.neutral');
+  const backwardButton = document.querySelector('.backward');
   const forwardButton = document.querySelector('.forward');
 
-  const backPress$ = createPressStream(backwardButton).pipe(mapTo(Command.Backward));
-  const neutralPress$ = createPressStream(neutralButton).pipe(mapTo(Command.Neutral));
-  const forwardPress$ = createPressStream(forwardButton).pipe(mapTo(Command.Forward));
+  const neutralPress$ = createPressStream(neutralButton!).pipe(mapTo(Command.Neutral));
+  const backPress$ = createPressStream(backwardButton!).pipe(mapTo(Command.Backward));
+  const forwardPress$ = createPressStream(forwardButton!).pipe(mapTo(Command.Forward));
 
   merge(backPress$, neutralPress$, forwardPress$)
-    .subscribe(console.log);
+    .subscribe(_label => {
+      const image = capture(webcamera);
+      console.log(image);
+    });
 };
 
 (async () => {
   setupUI();
-  const webcam = document.querySelector('#webcam') as HTMLVideoElement;
-  setupWebcamera(webcam);
 })();
