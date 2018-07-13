7ac0a939eeb91ef77cc7fa1378060dfad91bb979
chooblarin <choo.bla.rin@gmail.com>
2018-07-13 17:01:10 +0900

    we only need to run this in localhost. We don't need https anymore 👋

--- web/robot.ts
+++ web/robot.ts
@@ -14,7 +14,7 @@ export class RobotController {
   }
 
   static createInstance(topic$: Observable<string>): Promise<RobotController> {
-    const client = connect(`mqtts://${location.hostname}:9001`);
+    const client = connect(`mqtt://${location.hostname}:9001`);
     return new Promise((resolve, reject) =>
       client
         .on('connect', () => {
