73bb477a21e3fea91e1d4599528913ce1a0b3edd
chooblarin <choo.bla.rin@gmail.com>
2018-07-30 19:10:40 +0900

    feat: settings modal

--- web/pairplay.ts
+++ web/pairplay.ts
@@ -296,7 +296,7 @@ const changeTheme = (isDark: boolean) => {
 
 const setupThemeToggle = () => {
   const themeToggleSwitch = document.querySelector('.theme-toggle input')!;
-  const toggleContainer = document.querySelector('.toggle-button-container')!;
+  const themeToggle = document.querySelector('.theme-toggle')!;
 
   const checked$: Observable<boolean> = fromEvent(
     themeToggleSwitch,
@@ -309,9 +309,9 @@ const setupThemeToggle = () => {
 
   checked$.subscribe(isChecked => {
     if (isChecked) {
-      toggleContainer.classList.add('theme-checked');
+      themeToggle.classList.add('theme-checked');
     } else {
-      toggleContainer.classList.remove('theme-checked');
+      themeToggle.classList.remove('theme-checked');
     }
     changeTheme(isChecked);
   });
@@ -319,6 +319,26 @@ const setupThemeToggle = () => {
 
 const setupUI = async () => {
   setupThemeToggle();
+
+  const settingsButton = document.querySelector('.header .settings')!;
+  const settingsModal = document.querySelector('.modal-settings')!;
+
+  document
+    .querySelector('.modal-settings-content')!
+    .addEventListener('click', ev => ev.stopImmediatePropagation());
+
+  merge(
+    fromEvent(settingsButton, 'click').pipe(mapTo(true)),
+    fromEvent(settingsModal, 'click').pipe(mapTo(false))
+  )
+    .pipe(startWith(false))
+    .subscribe(isOpen => {
+      if (isOpen) {
+        settingsModal.classList.add('active');
+      } else {
+        settingsModal.classList.remove('active');
+      }
+    });
 
   videoLeft = document.querySelector(
     '.webcam-box.left video'
